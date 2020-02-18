1e903e36649e89636ca458aa9d3ff3d7a5880302
Barbara Whiting <whitingba@gmail.com>
2020-02-18 13:01:06 -0700

    random word generator created within an object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,36 @@
-// create an object for my guessing game
-var goGreenGame = {
-    // bank of words that will be chosen at random for user to guess
-    wordBank: ["straws", "plastic", "bags"],
-    // random word chosen for user to guess
-    randomWord: "",
-    // array of guesses made by the user
-    guessArray: [],
-    // TODO: add wins, losses and guesses remaining later
+// build game in an object
+
+// random word is chosen at start of game
+
+// User sees blanks where letters will be indicating the word to be guessed. Now they know the number of letters
+
+// User presses a key to input their guess and guess is checked to see if valid
+//   if correct it will be revealed in the word field and the letter will be displayed in the letter bank
+//   else, their number of available guesses will decrease and the letter they chose will display in the guessed letters letter bank
+
+// They will continue to guess until number of guesses has expired. 
+//    Game Over will be displayed
+//    Word will be revealed
+
+// if they guess the entire word correctly
+//   Message congratulating them will be displayed along with information about the word displayed
 
 
-    beginGame: function () {
-        // clear the guess array
-        this.guessArray = [];
-        this.setRandomWord();
+var goGreenWordGuess = {
+
+    words: [
+        "straws",
+        "plastic",
+        "bags",
+        "environment",
+    ],
+
+    getWord: function () {
+        var word = this.words[Math.floor(Math.random() * this.words.length)];
+        document.getElementById("wordDisplay").innerHTML = `<h3>${word}</h3>`;
     },
 
-    setRandomWord: function () {
-        this.randomWord = this.wordBank[Math.floor(Math.random() * this.wordBank.length)];
-    }
 
-}
+};
+
+
